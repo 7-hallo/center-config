33bcb0697c2b85ffaebf11ca6860bf2bdb6c9436
7zism3 <7zism3@gmail.com>
2024-07-02 16:28:54 +0700

    #setup - 1st commit

---
+++ src/main/java/com/sevenhallo/centerconfig/CenterconfigApplication.java
@@ -0,0 +1,13 @@
+package com.sevenhallo.centerconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenterconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenterconfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sevenhallo/centerconfig/CenterconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevenhallo.centerconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenterconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
